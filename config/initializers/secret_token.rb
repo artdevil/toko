d4f0001a8b4f5f914bdcd0679546c790c8e8dd97
adhiguna sabril <artdvl91@gmail.com>
2011-12-06 22:14:41 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toko::Application.config.secret_token = '62fc651d9ebc470b4932748972e18b62cd0aab80e76fa61c7f716cf50fcbf4f54c4217ad06eeaa0d5df69e804289a2319447549510872c4c31b4de569d34d458'
